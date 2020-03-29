7c55a35e81cf3fc6b4674e0c55d8c5666d7b11fe
Christopher Nilsson <christopher.nille.nilsson@gmail.com>
2020-03-29 20:39:05 +0200

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,23 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
-import Array
 import Browser
 import Browser.Dom
 import Data exposing (dataList)
 import Debug exposing (log)
-import Html exposing (Html, div, h1, img, input, span, text)
-import Html.Attributes exposing (class, id, placeholder, src, type_, value)
-import Html.Events exposing (onInput)
+import Html exposing (Html, button, div, h1, hr, img, input, span, text)
+import Html.Attributes exposing (class, disabled, id, placeholder, src, type_, value)
+import Html.Events exposing (onClick, onInput)
 import Task
+import Time
+
+
+
+---- UTIL ----
+
+
+getElementAtIndex : List a -> Int -> Maybe a
+getElementAtIndex list idx =
+    List.head (List.drop idx (List.take (idx + 1) list))
 
 
 
@@ -22,23 +31,32 @@ type WordState
 
 
 type alias Word =
-    { value : String, state : WordState }
+    { value : String
+    , state : WordState
+    , show : Bool
+    }
 
 
 type alias Model =
     { txt : String
     , wordList : List Word
     , index : Int
-    , offsetTop : Float
+    , offset : Float
+    , lineDone : Bool
+    , timeLeft : Int
+    , gameStarted : Bool
     }
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { txt = ""
-      , wordList = List.map (\word -> Word word Neutral) dataList
+      , wordList = List.map (\word -> Word word Neutral True) dataList
       , index = 0
-      , offsetTop = 0
+      , offset = 0
+      , lineDone = False
+      , timeLeft = 60
+      , gameStarted = False
       }
     , Cmd.none
     )
@@ -48,9 +66,19 @@ init =
 ---- UPDATE ---
 
 
-getElementAtIndex : List a -> Int -> Maybe a
-getElementAtIndex list idx =
-    List.head (List.drop idx (List.take (idx + 1) list))
+type Msg
+    = TextChanged String
+    | GotCurrentElement (Result Browser.Dom.Error Browser.Dom.Element)
+    | StartGame
+    | Tick Time.Posix
+    | NoOp
+
+
+jumpToOffset : Float -> Float -> Cmd Msg
+jumpToOffset y y2 =
+    Browser.Dom.setViewportOf "word-box" 0 (y - y2)
+        |> Task.onError (\_ -> Task.succeed ())
+        |> Task.perform (\_ -> NoOp)
 
 
 getCurrentWord : Model -> String
@@ -67,21 +95,17 @@ getCurrentWord model =
 
 handleTextChanged : String -> Model -> ( Model, Cmd Msg )
 handleTextChanged newTxt model =
-    if newTxt /= "nille är bäst " then
-        -- ( { model | txt = "" }, Cmd.none )
-        ( { model | txt = "", index = model.index + 1 }
-        , Task.attempt GotCurrentElement (Browser.Dom.getElement "current")
-        )
+    if not model.gameStarted then
+        ( { model | txt = "" }, Cmd.none )
 
     else if String.endsWith " " newTxt then
         if newTxt == (getCurrentWord model ++ " ") then
-            ( { model | txt = "", index = model.index + 1 }
+            ( { model | txt = "", index = model.index + 1, lineDone = False }
             , Task.attempt GotCurrentElement (Browser.Dom.getElement "current")
             )
 
         else
-            -- ( { model | txt = "" }
-            ( { model | txt = "", index = model.index + 1 }
+            ( { model | txt = "" }
             , Cmd.none
             )
 
@@ -89,27 +113,36 @@ handleTextChanged newTxt model =
         ( { model | txt = newTxt }, Cmd.none )
 
 
-type Msg
-    = TextChanged String
-    | GotCurrentElement (Result Browser.Dom.Error Browser.Dom.Element)
-    | NoOp
+hideOldWord : Int -> Int -> Word -> Word
+hideOldWord index i w =
+    if i < index then
+        { w | show = False }
+
+    else
+        w
 
 
-jumpToOffset : Float -> Float -> Cmd Msg
-jumpToOffset y y2 =
-    Browser.Dom.setViewportOf "word-box" 0 (y - y2)
-        |> Task.onError (\_ -> Task.succeed ())
-        |> Task.perform (\_ -> NoOp)
+hideOldWords : Model -> List Word
+hideOldWords model =
+    List.indexedMap
+        (hideOldWord model.index)
+        model.wordList
 
 
+offsetWordListIfNewLine : Model -> Browser.Dom.Element -> ( Model, Cmd Msg )
+offsetWordListIfNewLine model r =
+    let
+        lineDone =
+            r.element.y /= model.offset
 
---     Browser.Dom.getElement "word-box"
---         |> Task.andThen
---             (\info ->
---                 Browser.Dom.setViewportOf "word-box" 0 (y - y2)
---             )
---         |> Task.onError (\_ -> Task.succeed ())
---         |> Task.perform (\_ -> NoOp)
+        offset =
+            r.element.y
+    in
+    if lineDone && model.index > 2 then
+        ( { model | offset = offset, lineDone = lineDone, wordList = hideOldWords model }, Cmd.none )
+
+    else
+        ( { model | offset = offset, lineDone = lineDone }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -121,28 +154,70 @@ update msg model =
         GotCurrentElement result ->
             case result of
                 Ok r ->
-                    ( { model | offsetTop = r.viewport.y }
-                    , jumpToOffset r.element.y r.viewport.y
-                    )
+                    offsetWordListIfNewLine model r
 
                 Err _ ->
                     ( model, Cmd.none )
+
+        StartGame ->
+            ( { model | gameStarted = True }, Task.attempt (\_ -> NoOp) (Browser.Dom.focus "inputfield") )
+
+        Tick _ ->
+            let
+                timeLeft =
+                    model.timeLeft - 1
+            in
+            ( { model | timeLeft = timeLeft, gameStarted = timeLeft > 0 }
+            , Cmd.none
+            )
 
         NoOp ->
             ( model, Cmd.none )
 
 
 
+---- SUBSCRIPTIONS ----
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    if model.gameStarted && model.timeLeft >= 0 then
+        Time.every 1000 Tick
+
+    else
+        Sub.none
+
+
+
 ---- VIEW ----
 
 
-wordToSpan : Int -> Int -> Word -> Html Msg
-wordToSpan index current word =
-    if index == current then
-        span [ class "current", id "current" ] [ text word.value ]
+wordToSpan : String -> Int -> Int -> Word -> Html Msg
+wordToSpan currInput index current word =
+    if word.show then
+        if index == current then
+            span
+                [ class
+                    (if String.startsWith currInput word.value then
+                        "current"
+
+                     else
+                        "current wrong"
+                    )
+                , id "current"
+                ]
+                [ text word.value ]
+
+        else
+            span [] [ text word.value ]
 
     else
-        span [] [ text word.value ]
+        text ""
+
+
+getWordsFromModel : Model -> List (Html Msg)
+getWordsFromModel model =
+    List.indexedMap (wordToSpan model.txt model.index) model.wordList
 
 
 view : Model -> Html Msg
@@ -150,14 +225,17 @@ view model =
     div []
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "Your Elm App is working!" ]
-        , div [ class "word-box", id "word-box" ] (List.indexedMap (wordToSpan model.index) model.wordList)
-        , viewInput "text" "" model.txt TextChanged
+        , div [ class "word-box", id "word-box" ] (getWordsFromModel model)
+        , viewInput "text" "" model.txt "inputfield" TextChanged (not model.gameStarted)
+        , hr [] []
+        , button [ onClick StartGame, disabled model.gameStarted ] [ Html.text "Start game" ]
+        , Html.text (String.fromInt model.timeLeft ++ " seconds left")
         ]
 
 
-viewInput : String -> String -> String -> (String -> msg) -> Html msg
-viewInput t p v toMsg =
-    input [ type_ t, placeholder p, value v, onInput toMsg ] []
+viewInput : String -> String -> String -> String -> (String -> msg) -> Bool -> Html msg
+viewInput t p v i toMsg d =
+    input [ type_ t, placeholder p, value v, onInput toMsg, id i, disabled d ] []
 
 
 
@@ -170,5 +248,5 @@ main =
         { view = view
         , init = \_ -> init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
